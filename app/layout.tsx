ce2a561495c35f23598789e949525de962d7a089
PRASUN-SITAULA <psitaula209@gmail.com>
2024-11-11 11:23:03 +0545

    fixed layout error

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,17 +3,7 @@ import localFont from 'next/font/local'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import Navbar from '@/components/Navbar'
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-})
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-})
+import { geistSans, geistMono } from './fonts'
 
 export const metadata: Metadata = {
   title: 'ChitChat',
